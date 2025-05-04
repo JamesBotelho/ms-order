f53a9219c6aca63b355e81aa0d1d75c1a855c9b3
JamesBotelho <james_oliveira@live.com>
2025-05-04 11:16:46 -0300

    feat: add controller and body validation

--- src/main/java/dev/jamesbotelho/order/model/dto/AddressDTO.java
+++ src/main/java/dev/jamesbotelho/order/model/dto/AddressDTO.java
@@ -1,13 +1,19 @@
 package dev.jamesbotelho.order.model.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
 @Builder
 public record AddressDTO(
+  @NotBlank(message = "street is required")
   String street,
+  @NotBlank(message = "address number is required")
   String number,
+  @NotBlank(message = "postal code is required")
   String postalCode,
+  @NotBlank(message = "city is required")
   String city,
+  @NotBlank(message = "state is required")
   String state
 ) {
 }
--- src/main/java/dev/jamesbotelho/order/model/dto/ItemDTO.java
+++ src/main/java/dev/jamesbotelho/order/model/dto/ItemDTO.java
@@ -1,12 +1,19 @@
 package dev.jamesbotelho.order.model.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 import java.math.BigDecimal;
 
 @Builder
 public record ItemDTO(
+  @NotBlank(message = "product id is required")
   String productId,
+  @NotNull(message = "count products is required")
+  @Min(value = 1, message = "The min value of product is 1")
   Integer count,
+  @NotNull(message = "price is required")
   BigDecimal price
 ) {}
